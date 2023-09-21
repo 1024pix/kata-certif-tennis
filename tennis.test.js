cd5d36b53002a53d47141b08dab4934da3fc4d4d
Andreia Pena <andreia.pena@pix.fr>
2023-09-21 15:34:54 +0200

    ♻️ tennis: refacto tests

--- tennis.test.js
+++ tennis.test.js
@@ -22,47 +22,13 @@ describe("#Score", () => {
     assert.strictEqual(result, expected);
   });
 
-  describe("when given (0,1)", () => {
-    it('it displays "love - fifteen"', () => {
-      // given
-      const expected = "love - fifteen";
-      // when
-      const result = checkScore(0, 1);
+  [[0,1,"love - fifteen"], [0,2,"love - thirty"], [0,3,"love - fourty"], [1,0,"fifteen - love"]].forEach(([teamAScore,teamBScore,expected]) =>
+      describe(`when given (${teamAScore},${teamBScore})`, () => {
+    it(`it displays ${expected}`, () => {
+      // given when
+      const result = checkScore(teamAScore, teamBScore);
       // then
       assert.strictEqual(result, expected);
     });
-  });
-
-  describe("when given (0,2)", () => {
-    it('it displays "love - thirty"', () => {
-      // given
-      const expected = "love - thirty";
-      // when
-      const result = checkScore(0, 2);
-      // then
-      assert.strictEqual(result, expected);
-    });
-  });
-
-  describe("when given (0,3)", () => {
-    it('it displays "love - fourty"', () => {
-      // given
-      const expected = "love - fourty";
-      // when
-      const result = checkScore(0, 3);
-      // then
-      assert.strictEqual(result, expected);
-    });
-  });
-
-  describe("when given (1,0)", () => {
-      it('it displays "fifteen - love"', () => {
-        // given
-        const expected = "fifteen - love";
-        // when
-        const result = checkScore(1, 0);
-        // then
-        assert.strictEqual(result, expected);
-      });
-  });
+  }));
 });

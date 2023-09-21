6a2233ff60b1b85f5f4e5b4b2100175c7c3c7ae2
P-Jeremy <jemyplu@gmail.com>
2023-09-21 15:15:35 +0200

    add 3 tests and 3 ifs

--- tennis.test.js
+++ tennis.test.js
@@ -1,17 +1,62 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert';
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
 function checkScore(teamAScore, teamBScore) {
-  return 'deuce';
+  if (teamAScore === 0) {
+    if (teamBScore === 2) {
+      return "love - thirty";
+    }
+
+    if (teamBScore === 3) {
+      return "love - fourty";
+    }
+
+    return "love - fifteen";
+  }
+
+  return "deuce";
 }
 
-describe('#Score', () => {
-  it(`it displays the result`, () => {
+describe("#Score", () => {
+  it(`it displays a result`, () => {
     // given
-    const expected = 'deuce';
+    const expected = "deuce";
     // when
-    const result = checkScore(3, 3);
+    const result = checkScore();
     // then
     assert.strictEqual(result, expected);
+  });
+
+  describe("when given (0,1)", () => {
+    it('it displays "love - fifteen"', () => {
+      // given
+      const expected = "love - fifteen";
+      // when
+      const result = checkScore(0, 1);
+      // then
+      assert.strictEqual(result, expected);
+    });
+  });
+
+  describe("when given (0,2)", () => {
+    it('it displays "love - thirty"', () => {
+      // given
+      const expected = "love - thirty";
+      // when
+      const result = checkScore(0, 2);
+      // then
+      assert.strictEqual(result, expected);
+    });
+  });
+
+  describe("when given (0,3)", () => {
+    it('it displays "love - fourty"', () => {
+      // given
+      const expected = "love - fourty";
+      // when
+      const result = checkScore(0, 3);
+      // then
+      assert.strictEqual(result, expected);
+    });
   });
 });
